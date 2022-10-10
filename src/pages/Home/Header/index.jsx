b147b35c8be46be2c502777659cda1fe675ae4e7
Gabriel Arroyo <gabriel_eleazar.arroyo@daimler.com>
2022-10-10 15:14:54 -0500

    header video

--- src/pages/Home/Header/index.jsx
+++ src/pages/Home/Header/index.jsx
@@ -5,26 +5,20 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="masthead" id="page-top">
-      <div className="container px-5 header-text ">
-        <div className="header-text row gx-5 align-items-center">
-          <div className="col-md-12 col-lg-6 header-col">
-            <div className="mb-lg-5 mb-lg-0 text-center text-lg-start ">
-              <p className="title-1 text-primary welcome-title">Bienvenido</p>
-              <p className="welcome-text welcome-text">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Accusamus unde commodi sapiente dicta aut possimus similique,
-                voluptas quae ratione est. Dolor a natus porro aliquam dicta
-                delectus possimus autem aperiam?
-              </p>
-            </div>
-            <div className="button-container d-flex flex-column flex-lg-row align-items-center">
-              <Button className="text-light text-uppercase">
-                Cotizar Ahora
-              </Button>
-            </div>
-          </div>
-        </div>
+      <p className="title-1 text-primary welcome-title">Bienvenido</p>
+      <p className="welcome-text welcome-text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus unde
+        commodi sapiente dicta aut possimus similique, voluptas quae ratione
+        est. Dolor a natus porro aliquam dicta delectus possimus autem aperiam?
+      </p>
+      <div className="button-container d-flex flex-column flex-lg-row align-items-center">
+        <Button className="text-light text-uppercase">Cotizar Ahora</Button>
       </div>
+      <img
+        className="video"
+        src="https://nono-c.com/wp-content/uploads/2019/03/video-placeholder.jpg"
+        alt="video"
+      />
     </header>
   );
 };
